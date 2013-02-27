923c8b4de2258ea546fcd3229b8094af2bbf4674
Todd Wolfson <todd@twolfson.com>
2013-02-27 02:05:44 -0800

    Added more developer notes

--- background.js
+++ background.js
@@ -42,6 +42,8 @@ var appRequest = app.request,
 var button = appAPI.browserAction;
 button.setResourceIcon('grey_pin.png');
 
+// DEV: Give me the sexy. I want event emitter syntax for button and messaging
+
 button.onClick(function (e) {
   // TODO: Use alt+shift+click for options
   // TODO: Notify user about this via description and tooltip
