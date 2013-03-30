8b7dd3d849dc4f73b75c14ac7ae6d7845907c7d3
Todd Wolfson <todd@twolfson.com>
2013-03-30 00:05:09 -0700

    Moving to an app for static hosting

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var serve = require('serve');
-console.log(serve);
+// Load in express and create our server
+var express = require('express'),
+    app = express();
+
+// Host our files statically
+app.use(express['static'](__dirname + '/lib/'));
+
+// TODO: Open a /health route
+
+// Listen at 3000
+var port = 3000;
+app.listen(port);
+console.log('Server is running at http://localhost:' + port + '/');
