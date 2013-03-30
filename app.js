56530bc17812b6e36aa7604280c9644415e37a15
Todd Wolfson <todd@twolfson.com>
2013-03-30 00:23:04 -0700

    Building out more against dev environment

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 var express = require('express'),
     app = express();
 
+// Log requests as they come in
+app.use(express.logger('short'));
+
 // Host our files statically
 app.use(express['static'](__dirname + '/lib/'));
 
-// TODO: Open a /health route
+// Health page (attribution to dshaw)
+var pkg = require('./package.json');
+    version = pkg.version;
+app.get('/health', function (req, res) {
+  var health = {
+    'version': pkg.version,
+    'pid': process.pid,
+    'memory': process.memoryUsage(),
+    'uptime': process.uptime()
+  };
+  res.send(health);
+});
 
 // Listen at 3000
 var port = 3000;
